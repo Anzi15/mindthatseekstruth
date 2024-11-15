11c61a01b323beb4d5fc9252149d16f5fa4c36ef
Anzi <djam4343@gmail.com>
2024-11-15 16:22:56 +0500

    added header and navbar

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,19 @@
+    
+import Link from "next/link.js";
+
+function Navbar({ links }) {
+  return (
+    <div className="hidden lg:flex items-center gap-8 grow justify-center  ">
+      {links.map((link) => {
+        return (
+          <Link key={link.id} href={link.href} className="hover:underline underline-offset-2 ">
+            {link.name}
+          </Link>
+        );
+      })}
+      
+    </div>
+  );
+}
+
+export default Navbar;

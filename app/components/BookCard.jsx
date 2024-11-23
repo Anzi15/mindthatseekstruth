f4898b549ba5281f87afefa3cdba37a97ddf383f
Anzi <djam4343@gmail.com>
2024-11-23 23:28:02 +0500

    added books section

--- app/components/BookCard.jsx
+++ app/components/BookCard.jsx
@@ -1,21 +1,52 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const BookCard = ({bookImg, bookTitle, bookDescription}) => {
+const BookCard = ({ bookImg, bookTitle, bookDescription }) => {
   return (
-    <div>
-      
-<Link href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <Image className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={bookImg} alt={bookTitle} width={720} height={1080} />
-    <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{bookTitle}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{bookDescription}</p>
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <Link href="#">
+        <Image
+          className="rounded-t-lg"
+          src={bookImg}
+          alt={bookTitle}
+          width={400}
+          height={300}
+        />
+      </Link>
+      <div className="p-5">
+        <Link href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {bookTitle}
+          </h5>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {bookDescription}
+        </p>
+        <Link
+          href="#"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m-6-4v8"
+            />
+          </svg>
+        </Link>
+      </div>
     </div>
-</Link>
+  );
+};
 
-    </div>
-  )
-}
-
-export default BookCard
+export default BookCard;
--- app/sections/BookSection.jsx
+++ app/sections/BookSection.jsx
@@ -1,11 +1,65 @@
 import { Section } from 'lucide-react'
 import React from 'react'
 import BookCard from '../components/BookCard'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const BookSection = () => {
   return (
-    <section>
-      <BookCard bookTitle={"Me, My Phsyche and I"} bookImg={"https://i.ibb.co/5Mjy2LK/Mini-Sample-Book-Book-2-Me-My-Psyche-and-I-page-001.jpg"} bookDescription={"Dive deep into the uncharted territories of the human mind with Me, My Psyche, and I, a thought-provoking and introspective exploration of self-discovery, mental health, and personal growth. This compelling book takes readers on a journey through the intricate labyrinth of emotions, memories, and the psyche, offering profound insights into understanding oneself and navigating the complexities of life."}/>
+    <section className="flex flex-col justify-center items-center py-10">
+     <h2 className="md:text-4xl text-2xl font-bold uppercase my-4">Our books</h2>
+
+     <div className="flex justify-center items-center m-auto py-10 gap-4 flex-col md:flex-row">
+        <div className="md:w-1/3 p-8 md:0">
+        <h3 className="title text-2xl font-semibold py-2">
+            Me, My Psyche and I
+        </h3>
+        <p className="text-gray-500">
+        Dive deep into the uncharted territories of the human mind with Me, My Psyche, and I, a thought-provoking and introspective exploration of self-discovery, mental health, and perso...
+        </p>
+        </div>
+        <div className="md:w-1/2">
+        <Image src="https://i.ibb.co/mcWJfdc/smartmockups-m3ug5rw0.jpg" alt="Me, My Psyche and I" width="720" height="1080" className="rounded" />
+        </div>
+     </div>
+
+
+     <div className="flex justify-center items-center m-auto py-10 gap-4 flex-col-reverse md:flex-row-reverse">
+        <div className="md:w-1/3 p-8 md:0">
+        <h3 className="title py-2 text-2xl font-semibold">
+            The Break up Pain
+        </h3>
+        <p className="text-gray-500">
+        The Breakup Pain is a poignant and emotionally charged book that delves into the raw and often overwhelming experience of heartbreak. Through a blend of personal stories, expert advice...
+        </p>
+        </div>
+        <div className="md:w-1/2">
+        <Image src="https://i.ibb.co/G9Y8RFy/mediamodifier-image-1.png" alt="Me, My Psyche and I" width="720" height="1080" className="rounded-lg" />
+        </div>
+     </div>
+
+     <div className="flex justify-center items-center m-auto py-10 gap-4 flex-col md:flex-row">
+        <div className="md:w-1/3 p-8 md:0">
+        <h3 className="title text-2xl font-semibold py-2">
+        Transient thoughts and Me!
+        </h3>
+        <p className="text-gray-500">
+        a captivating journey through the fleeting and often elusive nature of human thoughts, emotions, and introspection. In this book, the author explores the transient nature of our minds and how we relate to our inner dia...
+        </p>
+        </div>
+        <div className="md:w-1/2">
+        <Image src="https://i.ibb.co/VvxVgsq/mediamodifier-image.png" alt="Me, My Psyche and I" width="720" height="1080" className="" />
+        </div>
+     </div>
+
+<Link
+href="/books"
+className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden  font-normal text-gray-600 rounded-lg group bg-gradient-to-br from-violet-600 to-blue-400 group-hover:from-indigo-600 group-hover:to-indigo-500 hover:text-white text-2xl my-10">
+<span
+  className="relative py-2 px-5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
+  View all books
+</span>
+</Link>
     </section>
   )
 }
